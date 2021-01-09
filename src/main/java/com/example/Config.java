01bcf34f9361218481ded0bb906b47e479428e62
Thanh Vu <thanhauco@gmail.com>
2021-01-09 15:06:00 -0800

    Load config from env

--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -3,7 +3,7 @@ import java.util.Properties;
 public class Config {
     public static Properties load() {
         Properties p = new Properties();
-        p.put("bootstrap.servers", "localhost:9092");
+        p.put("bootstrap.servers", System.getenv().getOrDefault("KAFKA_BROKERS", "localhost:9092"));
         p.put("group.id", "stream-pipe");
         p.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         p.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
