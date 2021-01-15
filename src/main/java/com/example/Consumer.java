91f7e7d4e789cfedc457a655493e7db9bc8b7506
Thanh Vu <thanhauco@gmail.com>
2021-01-15 11:23:00 -0800

    Implement subscribe

--- src/main/java/com/example/Consumer.java
+++ src/main/java/com/example/Consumer.java
@@ -1,7 +1,9 @@
 package com.example;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import java.util.Collections;
 import java.util.Properties;
 public class Consumer {
     private final KafkaConsumer<String, String> consumer;
     public Consumer(Properties p) { consumer = new KafkaConsumer<>(p); }
+    public void subscribe(String topic) { consumer.subscribe(Collections.singletonList(topic)); }
 }
