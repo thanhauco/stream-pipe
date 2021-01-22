c5c1aed5891e576fdb5618e44d98259f26953c78
Thanh Vu <thanhauco@gmail.com>
2021-01-22 02:06:00 -0800

    Refactor Consumer to accept callback

--- src/main/java/com/example/Consumer.java
+++ src/main/java/com/example/Consumer.java
@@ -4,14 +4,13 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
-public class Consumer {
+import java.util.function.Consumer;
+public class ConsumerWrapper {
     private final KafkaConsumer<String, String> consumer;
-    public Consumer(Properties p) { consumer = new KafkaConsumer<>(p); }
+    public ConsumerWrapper(Properties p) { consumer = new KafkaConsumer<>(p); }
     public void subscribe(String topic) { consumer.subscribe(Collections.singletonList(topic)); }
-    public void run() {
-        while(true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            records.forEach(r -> System.out.println(r.value()));
-        }
+    public void poll(Consumer<String> callback) {
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        records.forEach(r -> callback.accept(r.value()));
     }
 }
