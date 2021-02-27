543f024dbc0208a23c672b3e3fe8f480398dc8a5
Thanh Vu <thanhauco@gmail.com>
2021-02-27 07:33:00 -0800

    Add Windowed Count

--- src/main/java/com/example/StreamApp.java
+++ src/main/java/com/example/StreamApp.java
@@ -1,9 +1,11 @@
 package com.example;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import java.time.Duration;
 public class StreamApp {
     public void build(StreamsBuilder builder) {
         KStream<String, String> source = builder.stream("input");
-        source.mapValues(v -> v.toUpperCase()).to("output");
+        source.groupByKey().windowedBy(TimeWindows.of(Duration.ofMinutes(1))).count();
     }
 }
