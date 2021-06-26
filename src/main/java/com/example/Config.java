7a861cbb3e9fd9923d351123bdd83e761e4e19e0
Thanh Vu <thanhauco@gmail.com>
2021-06-26 03:05:00 -0700

    Add Env Var Config

--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -1,7 +1,7 @@
 package com.example;
-import java.util.Properties;
 public class Config {
-    private Properties p = new Properties();
-    public Config bootstrap(String s) { p.put("bootstrap.servers", s); return this; }
-    public Properties build() { return p; }
+    public String getEnv(String key, String def) {
+        String val = System.getenv(key);
+        return val != null ? val : def;
+    }
 }
