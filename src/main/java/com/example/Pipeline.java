9c1e9d5051d07a3a9b893f7d8f8b13aaf157d8ea
Thanh Vu <thanhauco@gmail.com>
2021-07-04 08:08:00 -0700

    Add Graceful Shutdown

--- src/main/java/com/example/Pipeline.java
+++ src/main/java/com/example/Pipeline.java
@@ -1,10 +1,9 @@
 package com.example;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 public class Pipeline {
-    private final ExecutorService pool = Executors.newFixedThreadPool(4);
     // ... existing
-    public void start() {
-        consumer.poll(msg -> pool.submit(() -> processor.process(msg)));
+    public void stop() throws InterruptedException {
+        pool.shutdown();
+        pool.awaitTermination(5, TimeUnit.SECONDS);
     }
 }
